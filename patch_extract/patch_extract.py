fd3867a45aa756e8902992050b4f65a4b79b8c81
codingchild <pshpulip40@gmail.com>
2023-08-12 00:22:56 +0900

    Update patch_extract.py

--- patch_extract/patch_extract.py
+++ patch_extract/patch_extract.py
@@ -15,18 +15,26 @@ class PatchExtractor:
         os.makedirs(save_folder, exist_ok=True)
 
         img = Image.open(img_path)
-
         w, h = img.size
         patch_count = 0
 
         for i in range(0, w, self.patch_size[0]):
             for j in range(0, h, self.patch_size[1]):
                 try:
-                    patch = img.crop((i, j, i + self.patch_size[0], j + self.patch_size[1]))
-                    patch_arr = np.array(patch.convert("RGB"))
+                    # 패치 크기 계산 (경계를 벗어나는 경우 크기 조정)
+                    patch_width = min(self.patch_size[0], w - i)
+                    patch_height = min(self.patch_size[1], h - j)
+
+                    patch = img.crop((i, j, i + patch_width, j + patch_height))
+                    padded_patch = Image.new("RGB", self.patch_size, (255, 255, 255))
+                    padded_patch.paste(patch, (0, 0, patch_width, patch_height))
+
+                    patch_arr = np.array(padded_patch)
 
                     if np.mean(patch_arr) < self.threshold:
                         patch_count += 1
-                        patch.save(os.path.join(save_folder, f'patch_{patch_count}.jpg'))
+                        patch_path = os.path.join(save_folder, f'patch_{patch_count}.jpg')
+                        padded_patch.save(patch_path)
                 except OSError:
                     continue
+        return patch_count
